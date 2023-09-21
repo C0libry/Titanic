1122ddb374cb8f190f53168010c55e8d24fe0f5b
C0libry <pe.pe.l@yandex.ru>
2023-09-21 19:33:51 +0300

    Added project presentation to home page

---
+++ resources/lang/en/home.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Welcome' => 'Welcome',
+    'info-chat' => 'Here you can communicate with other users and effectively manage your tasks.',
+    'info-tasks' => 'Our platform provides a user-friendly interface for creating, tracking and completing tasks. You can assign responsible participants and mark completed tasks.',
+    'Source code' => 'This is an open source project. The code is available on',
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,43 @@
 @endsection
 
 @section('main_content')
+    <div class="home">
+        <div class="intro home__intro">
+            <h1 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">{{ __('home.Welcome') }}</h1>
+            <div class="intro__info">
+                <img src="img/chat.png">
+                <div>{{ __('home.info-chat') }}</div>
+            </div>
+            <div class="intro__info">
+                <div>{{ __('home.info-tasks') }}</div>
+                <img src="img/tasks.png">
+            </div>
+            <div>{{ __('home.Source code') }}
+                <a href="https://github.com/C0libry/Titanic">GitHub</a>
+            </div>
+
+            <a class="cta cta--home" href="{{ route('register') }}">{{ __('menu.Sign Up') }}</a>
+
+            <div class="contacts">
+
+                <div class="contacts__info">
+                    <div class="contacts__info-block">
+                        <ion-icon name="call-outline"></ion-icon>
+                        <div class="contacts__info-text">+7 978 110 59 79</div>
+                    </div>
+
+                    <div class="contacts__info-block">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <div class="contacts__info-text">pe.pe.l@yandex.ru</div>
+                    </div>
+                </div>
+
+                <div class="contacts__icons">
+                    <a href="https://github.com/C0libry/"><ion-icon name="logo-github"></ion-icon></a>
+                    <a href="https://vk.com/c0libry"><ion-icon name="logo-vk"></ion-icon></a>
+                    <a href=""><ion-icon name="logo-linkedin"></ion-icon></a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
